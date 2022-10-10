3a84d7cc668ab50b3f4ee058d56f367bfa180e9e
Tomohiko Ozawa <kota65535@gmail.com>
2022-10-10 14:57:08 +0900

    fix parser

--- dist/index.js
+++ dist/index.js
@@ -15198,6 +15198,7 @@ const main = async () => {
   core.setOutput('output', jsonString(result.output));
   core.setOutput('warning', jsonString(result.warning));
   core.setOutput('summary', jsonString(result.summary));
+  core.setOutput('should-apply', result.shouldApply);
 };
 
 module.exports = main;
@@ -15279,7 +15280,7 @@ const getResourceActionSection = (inputLines) => {
   }
   return {
     offset,
-    sections: sections
+    sections
   };
 };
 
@@ -15291,6 +15292,11 @@ const getOutputChangeSection = (inputLines) => {
   const str = lines.join('\n');
   return {
     offset,
+    sections: findSections(lines,
+      /^\s{2}[+~-]\s(?<name>.*?)\s=/,
+      /(^\s{2}[+~-]\s(?<name>.*?)\s=)|(^$)/,
+      true
+    ),
     str
   };
 };
@@ -15317,25 +15323,20 @@ const getSummarySection = (inputLines) => {
         add: parseInt(match[1]),
         change: parseInt(match[2]),
         destroy: parseInt(match[3]),
-        str: match[0],
-        hasChanges: true
+        str: match[0]
       };
     }
   }
   {
     let { offset, match } = findLine(inputLines, /^No changes. Your infrastructure matches the configuration.$/);
-    if (match) {
-      return {
-        offset,
-        add: [],
-        change: [],
-        destroy: [],
-        str: match[0],
-        hasChanges: false
-      };
-    }
+    return {
+      offset,
+      add: 0,
+      change: 0,
+      destroy: 0,
+      str: match ? match[0] : ''
+    };
   }
-  throw new Error('Failed to get summary section');
 };
 
 const parse = (rawLines) => {
@@ -15351,12 +15352,21 @@ const parse = (rawLines) => {
   const warning = getWarningSection(lines);
   const summary = getSummarySection(lines);
 
+  const shouldApply = summary.add > 0 || summary.change > 0 || summary.destroy > 0 || output.sections.length > 0;
+
+  // Handle empty summary string when we have output changes but no resource changes
+  if (summary.str === '' && output.sections.length > 0) {
+    summary.offset = output.offset
+    summary.str = `Output Changes: ${output.sections.length}`;
+  }
+
   return {
     outside,
     action,
     output,
     warning,
-    summary
+    summary,
+    shouldApply
   };
 };
 
@@ -15410,7 +15420,7 @@ const findLinesBetween = (lines, beginPattern, endPattern) => {
   };
 };
 
-const findSections = (lines, beginPattern, endPattern) => {
+const findSections = (lines, beginPattern, endPattern, includeEndPattern) => {
   let ret = [];
   let groups = {};
   let str = null;
@@ -15423,6 +15433,15 @@ const findSections = (lines, beginPattern, endPattern) => {
           ...groups,
           str
         });
+        if (includeEndPattern) {
+          const m = line.match(beginPattern);
+          if (m) {
+            groups = m.groups;
+            str = line + '\n';
+            inside = true;
+            continue
+          }
+        }
         groups = {};
         str = null;
         inside = false;
--- src/parser.js
+++ src/parser.js
@@ -69,7 +69,7 @@ const getResourceActionSection = (inputLines) => {
   }
   return {
     offset,
-    sections: sections
+    sections
   };
 };
 
@@ -81,6 +81,11 @@ const getOutputChangeSection = (inputLines) => {
   const str = lines.join('\n');
   return {
     offset,
+    sections: findSections(lines,
+      /^\s{2}[+~-]\s(?<name>.*?)\s=/,
+      /(^\s{2}[+~-]\s(?<name>.*?)\s=)|(^$)/,
+      true
+    ),
     str
   };
 };
@@ -107,25 +112,20 @@ const getSummarySection = (inputLines) => {
         add: parseInt(match[1]),
         change: parseInt(match[2]),
         destroy: parseInt(match[3]),
-        str: match[0],
-        hasChanges: true
+        str: match[0]
       };
     }
   }
   {
     let { offset, match } = findLine(inputLines, /^No changes. Your infrastructure matches the configuration.$/);
-    if (match) {
-      return {
-        offset,
-        add: [],
-        change: [],
-        destroy: [],
-        str: match[0],
-        hasChanges: false
-      };
-    }
+    return {
+      offset,
+      add: 0,
+      change: 0,
+      destroy: 0,
+      str: match ? match[0] : ''
+    };
   }
-  throw new Error('Failed to get summary section');
 };
 
 const parse = (rawLines) => {
@@ -141,12 +141,21 @@ const parse = (rawLines) => {
   const warning = getWarningSection(lines);
   const summary = getSummarySection(lines);
 
+  const shouldApply = summary.add > 0 || summary.change > 0 || summary.destroy > 0 || output.sections.length > 0;
+
+  // Handle empty summary string when we have output changes but no resource changes
+  if (summary.str === '' && output.sections.length > 0) {
+    summary.offset = output.offset
+    summary.str = `Output Changes: ${output.sections.length}`;
+  }
+
   return {
     outside,
     action,
     output,
     warning,
-    summary
+    summary,
+    shouldApply
   };
 };
 
