090932d1192178fafe00b99e316d7f44666e86f6
Tomohiko Ozawa <kota65535@gmail.com>
2023-04-06 14:31:07 +0900

    a

--- dist/index.js
+++ dist/index.js
@@ -15720,7 +15720,7 @@ const getJobLogs = async (job, context) => {
   // get job logs
   const res2 = await axios.get(res.url);
 
-  return res2.data.split("\r\n");
+  return res2.data.split("\n");
 };
 
 const getContent = async (path, context) => {
